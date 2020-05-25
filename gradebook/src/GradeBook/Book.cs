e35e13153cacf1a92e255934078e20f97f22744f
Bruno Sanguineti <sanguineti.bruno@pwc.com>
2020-05-24 21:57:19 -0300

    Adding Construction method and Field Declaration

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,13 +1,22 @@
+using System.Collections.Generic;
+
 namespace GradeBook
 {
     class Book
     {
+        // Add Constructor for initializes grades. No return type. Has the same name that the Class.
+        public Book()
+        {
+            grades = new List<double>();
+        }
+
         public void AddGrade(double grade)
         {
-            if (grade > 0)
-            {
-                
-            }
+            grades.Add(grade);
         }
+
+        // Field Declaration
+        List<double> grades;    
+
     }
 }
