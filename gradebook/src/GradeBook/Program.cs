4ae0475327cba812d703c7e176fa9544a0314c04
Bruno Sanguineti <sanguineti.bruno@pwc.com>
2020-10-16 11:18:09 -0300

    Disabling Static Learn with a greeting example. Adding Writeline to show stadistics

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -11,15 +11,21 @@ namespace GradeBook
             book.addGrade(8.5);
             book.addGrade(8.75);
             book.addGrade(3.75);
+
+            var stats = book.getStadistics();
+
+            Console.WriteLine($"The lowest grade is {stats.lowValue}");
+            Console.WriteLine($"The highest grade is {stats.highValue}");
+            Console.WriteLine($"The average grade is {stats.average:N1}");
             //book.showStadistics();
 
             // I Cant Use a method without instance "Greeting.GreetingPublicVoidMethod();" dont work without a new instance
             // But work generating a new instance first
-            Greeting SaludoNewObject = new Greeting();
-            SaludoNewObject.GreetingPublicVoidMethod();
+            // Greeting SaludoNewObject = new Greeting();
+            // SaludoNewObject.GreetingPublicVoidMethod();
 
-            //A static method can be called without instance
-            Greeting.GreetingPublicStaticMethod();
+            // A static method can be called without instance
+            // Greeting.GreetingPublicStaticMethod();
 
         }
 
