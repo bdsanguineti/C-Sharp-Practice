618c2d02df3600d2dfc86f919fd29408c657d7fd
Bruno Sanguineti <sanguineti.bruno@pwc.com>
2020-10-16 09:19:25 -0300

    Changed Method camelCasing

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -8,10 +8,10 @@ namespace GradeBook
         static void Main(string[] args) 
         {
             var book = new Book("Bruce's Grade Book");
-            book.AddGrade(8.5);
-            book.AddGrade(8.75);
-            book.AddGrade(3.75);
-            book.ShowStadistics();
+            book.addGrade(8.5);
+            book.addGrade(8.75);
+            book.addGrade(3.75);
+            //book.showStadistics();
 
             // I Cant Use a method without instance "Greeting.GreetingPublicVoidMethod();" dont work without a new instance
             // But work generating a new instance first
