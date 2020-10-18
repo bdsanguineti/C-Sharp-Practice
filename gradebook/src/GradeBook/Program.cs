bb9fcabfb8eca3cbb01ec69242700abb48b04100
Bruno Sanguineti <sanguineti.bruno@pwc.com>
2020-10-18 02:21:05 -0300

    Unsubscribing Event

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -9,9 +9,6 @@ namespace GradeBook
         {
             var book = new Book("Bruce's Grade Book");
             book.GradeAdded += OnGradeAdded;
-            book.GradeAdded += OnGradeAdded;
-            book.GradeAdded -= OnGradeAdded;
-            book.GradeAdded += OnGradeAdded;
 
             while(true)
             {
